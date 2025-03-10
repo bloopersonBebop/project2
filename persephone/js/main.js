e6a972ceb8f9d4dd65424b04c19ff73ddd0aacd1
bloopersonBebop <amart991@gmail.com>
2025-03-10 03:48:10 -0700

    project2

---
+++ persephone/js/main.js
@@ -0,0 +1,47 @@
+let img0 = document.querySelector("#image-0");
+let img1 = document.querySelector("#image-1");
+let img2 = document.querySelector("#image-2");
+let img3 = document.querySelector("#image-3");
+let img4 = document.querySelector("#image-4");
+let img5 = document.querySelector("#image-5");
+let img6 = document.querySelector("#image-6");
+let img7 = document.querySelector("#image-7");
+let img8 = document.querySelector("#image-8");
+
+
+img0.addEventListener('click', () => {
+    img0.style.visibility = "hidden";
+});
+
+img1.addEventListener('click', () => {
+    img1.style.visibility = "hidden";
+});
+
+img2.addEventListener('click', () => {
+    img2.style.visibility = "hidden";
+});
+
+img3.addEventListener('click', () => {
+    img3.style.visibility = "hidden";
+});
+
+img4.addEventListener('click', () => {
+    img4.style.visibility = "hidden";
+});
+
+img5.addEventListener('click', () => {
+    img5.style.visibility = "hidden";
+});
+
+img6.addEventListener('click', () => {
+    img6.style.visibility = "hidden";
+});
+
+img7.addEventListener('click', () => {
+    img7.style.visibility = "hidden";
+});
+
+img8.addEventListener('click', () => {
+    img8.style.visibility = "hidden";
+});
+
